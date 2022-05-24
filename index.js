a791611699f8899a9028956a5f0a07455c83836b
Peter Work Laptop <petervsjim@gmail.com>
2022-05-24 17:20:23 +0800

    add get api

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const express = require("express");
 const app = express();
 app.use(express.static('public'));
+app.get('/', (req, res) => {
+  console.log("GET method worked");
+  return res.send('Received a GET HTTP method');
+});
 app.listen(3000, () => console.log("Server running on port 3000"));
